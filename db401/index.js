7d753c822b96d7707186d6cd76126873f6c2abf9
Tom <126760913+tomnotthomas@users.noreply.github.com>
2023-05-04 10:08:55 +0000

    query theaters per state

--- db401/index.js
+++ db401/index.js
@@ -5,50 +5,24 @@ const Movie = require('./models/movie')
 const Theater = require('./models/theater')
 
 async function main() {
-  const theatersByState = await Theater.aggregate([
+  // console.log(
+  //   await Movie.find({ 'imdb.rating': { $gt: 1 } })
+  //     .sort('-imdb.rating')
+  //     .limit(30)
+  // )
+
+  //const res = await Movie.aggregate([
+  //  { $match: { 'imdb.rating': { $gt: 1 } } },
+  //  { $sort: { 'imdb.rating': -1 } },
+  //  { $limit: 30 },
+  //])
+
+  const res = await Theater.aggregate([
     { $group: { _id: '$location.address.state', theaters: { $push: '$theaterId' }, numberOfTheaters: { $sum: 1 } } },
-    { $set: { state: '$_id' } },
-    { $unset: '_id' },
     { $sort: { numberOfTheaters: -1 } },
-    {
-      $group: {
-        _id: 1,
-        mostCrowdedState: { $first: '$state' },
-        mostCrowdedCount: { $first: '$numberOfTheaters' },
-        leastCrowdedState: { $last: '$state' },
-        leastCrowdedCount: { $last: '$numberOfTheaters' },
-      },
-    },
-    {
-      $project: {
-        _id: 0,
-        mostCrowdedState: {
-          name: '$mostCrowdedState',
-          numberOfTheaters: '$mostCrowdedCount',
-        },
-        leastCrowdedState: {
-          name: '$leastCrowdedState',
-          numberOfTheaters: '$leastCrowdedCount',
-        },
-      },
-    },
   ])
 
-  console.log(theatersByState)
-
-  const popularmoviesByDayOfWeek = await Movie.aggregate([
-    {
-      $group: {
-        _id: { $isoDayOfWeek: '$released' },
-        count: { $sum: 1 },
-        avgRating: { $avg: '$imdb.rating' },
-        stdDev: { $stdDevPop: '$imdb.rating' },
-      },
-    },
-    { $sort: { avgRating: -1 } },
-  ])
-
-  console.log(popularmoviesByDayOfWeek)
+  console.log(res)
 }
 
 main()
