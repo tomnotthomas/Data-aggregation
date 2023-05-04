cd85aefe22184d3d68cadb4a757cdb8892c95305
Tom <126760913+tomnotthomas@users.noreply.github.com>
2023-05-04 17:03:25 +0000

    print the answers for question 1 and 2 to the screen in the right order (first question1, then question2.

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const Comment = require('./models/comment')
 const Movie = require('./models/movie')
 const Theater = require('./models/theater')
 
-async function main() {
+async function firstQuestion() {
   const topMovies = await Movie.aggregate([
     //QUESTION1:which are the 3 top-rated movies for each genre, based on the average rating?
 
-    /*   { $unwind: '$genres' },
+    { $unwind: '$genres' },
     { $group: { _id: { genre: '$genres', title: '$title' }, rating: { $avg: '$imdb.rating' } } },
     { $sort: { '_id.genre': 1, rating: -1 } },
     {
@@ -20,11 +20,15 @@ async function main() {
     { $project: { topThreeMovies: { $slice: ['$movies', 3] } } },
   ])
 
-  console.log(JSON.stringify(topMovies, null, 2))
-}*/
+  console.log('QUESTION1:', JSON.stringify(topMovies, null, 2))
+}
 
-    //QUESTION2: Which genre has the highest average ratings?
+firstQuestion()
 
+//QUESTION2:what's the best genre based on average rating for the movies in that genre?
+async function secondQuestion() {
+  await firstQuestion()
+  const topGenre = await Movie.aggregate([
     { $unwind: '$genres' },
     { $group: { _id: { genre: '$genres' }, rating: { $avg: '$imdb.rating' } } },
 
@@ -37,7 +41,6 @@ async function main() {
     { $sort: { avgRating: -1 } },
     { $limit: 1 },
   ])
-  console.log(topMovies)
+  console.log('QUESTION2:', topGenre)
 }
-
-main()
+secondQuestion()
