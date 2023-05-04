4ca196fb7b8ab6083ec4893d2500b7d23b6a9045
Tom <126760913+tomnotthomas@users.noreply.github.com>
2023-05-04 16:45:03 +0000

    delete commented out code from lecture and leave answers to question1: 3 top-rated movies for each genre based on avg. rating and question 2: which genre has teh highest average ratings

--- index.js
+++ index.js
@@ -5,59 +5,9 @@ const Movie = require('./models/movie')
 const Theater = require('./models/theater')
 
 async function main() {
-  // console.log(
-  //   await Movie.find({ 'imdb.rating': { $gt: 1 } })
-  //     .sort('-imdb.rating')
-  //     .limit(30)
-  // )
-
-  //const res = await Movie.aggregate([
-  //  { $match: { 'imdb.rating': { $gt: 1 } } },
-  //  { $sort: { 'imdb.rating': -1 } },
-  //  { $limit: 30 },
-  //])
-
-  /*const theaters = await Theater.aggregate([
-    {
-      $group: {
-        _id: '$location.address.state',
-        theaters: { $push: '$theaterId' },
-        numberOfTheaters: { $sum: 1 },
-      },
-    },
-    { $set: { state: '$_id' } },
-    { $sort: { numberOfTheaters: -1 } },
-    {
-      $group: {
-        _id: 1,
-        mostCrowdedState: { $first: '$state' },
-        mostCrowdedCount: { $first: '$numberOfTheaters' },
-        leastCrowdedState: { $last: '$state' },
-        leastCrowdedCount: { $last: '$numberOfTheaters' },
-      },
-    },
-    {
-      $project: {
-        _id: 0,
-        mostCrowdedState: {
-          name: '$mostCrowdedState',
-          count: '$mostCrowdedCount',
-        },
-        leastCrowdedState: {
-          name: '$leastCrowdedState',
-          count: '$leastCrowdedCount',
-        },
-      },
-    },
-  ])
-*/
-  /*const movies = await Movie.aggregate([
-    { $group: { _id: { $isoDayOfWeek: '$released' }, count: { $sum: 1 }, avgRating: { $avg: '$imdb.rating' } } },
-    { $sort: { avgRating: -1 } },
-  ])
-*/
-
   const topMovies = await Movie.aggregate([
+    //QUESTION1:which are the 3 top-rated movies for each genre, based on the average rating?
+
     /*   { $unwind: '$genres' },
     { $group: { _id: { genre: '$genres', title: '$title' }, rating: { $avg: '$imdb.rating' } } },
     { $sort: { '_id.genre': 1, rating: -1 } },
@@ -72,6 +22,8 @@ async function main() {
 
   console.log(JSON.stringify(topMovies, null, 2))
 }*/
+
+    //QUESTION2: Which genre has the highest average ratings?
 
     { $unwind: '$genres' },
     { $group: { _id: { genre: '$genres' }, rating: { $avg: '$imdb.rating' } } },
